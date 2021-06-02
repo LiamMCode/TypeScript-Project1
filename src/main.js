70296f85902a3eef61baa2f4f1dc44408787e846
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-06-02 09:27:09 +0100

    created a simple project

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
-var message = 'Hello World';
-console.log(message);
+var Car = /** @class */ (function () {
+    function Car(engine) {
+        this.engine = engine;
+    }
+    Car.prototype.start = function () {
+        alert('Engine Started: ' + this.engine);
+    };
+    Car.prototype.stop = function () {
+        alert('Engine Stopped: ' + this.engine);
+    };
+    return Car;
+}());
+window.onload = function () {
+    var car = new Car('V8');
+    car.start();
+    car.stop();
+};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,21 @@
-let message: string = 'Hello World';
-console.log(message);
+class Car {
+    engine: string;
+
+    constructor(engine: string) {
+        this.engine = engine;
+    }
+
+    start() {
+        alert('Engine Started: ' + this.engine);
+    }
+
+    stop() {
+        alert('Engine Stopped: ' + this.engine);
+    }
+}
+
+window.onload = function () {
+    var car = new Car('V8');
+    car.start();
+    car.stop();
+}
